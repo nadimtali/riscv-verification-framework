d353dff17cfa9982d4acda239c4146845479a973
Nadim Tali <nadim.tali@mail.huji.ac.il>
2026-05-15 18:39:07 +0300

    Clean project structure and ignore build artifacts

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,7 +3,7 @@
 mkdir -p waves
 
 echo "Compiling ALU testbench..."
-iverilog -g2012 -o tb_alu.out rtl/riscv_alu.sv tb/tb_alu.sv
+iverilog -g2012 -o build/tb_alu.out rtl/riscv_alu.sv tb/tb_alu.sv
 
 echo "Running simulation..."
-vvp tb_alu.out
+vvp build/tb_alu.out
